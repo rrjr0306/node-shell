1ff361891377bbff4223a8d47c6fe61c65e642f1
rrjr0306 <rrjr0306@gmail.com>
2022-12-05 23:05:17 -0500

    Finalize project

--- curl.js
+++ curl.js
@@ -1,2 +1,9 @@
+
 const request = require('request')
 
+const _url = 'https://www.google.com';
+
+request(_url, {json: true }, (err, res, body) => {
+    if (err) { throw err; }
+    console.log(body)
+})
